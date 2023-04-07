77a30c4ede22ffc61c1611557976879f9edc78fa
Harish Kumar Verma <VERM396533@exactsoftware.com>
2023-04-07 11:58:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxWheelComponent, TextAlignment, TextOrientation } from 'ngx-wheel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'wheel-game';
+  @ViewChild(NgxWheelComponent, { static: false }) wheel: any;
+
+  seed = [...Array(12).keys()];
+  idToLandOn: any;
+
+  items: any[] = [];
+
+  textOrientation: TextOrientation = TextOrientation.HORIZONTAL;
+  textAlignment: TextAlignment = TextAlignment.OUTER;
+
+  ngOnInit() {
+    this.idToLandOn = this.seed[Math.floor(Math.random() * this.seed.length)];
+    const colors = ['#FF0000', '#000000'];
+    this.items = this.seed.map((value) => ({
+      fillStyle: colors[value % 2],
+      text: `Prize ${value}`,
+      id: value,
+      textFillStyle: 'white',
+      textFontSize: '16',
+    }));
+  }
+  reset() {
+    this.wheel.reset();
+  }
+  before() {
+    alert('Your wheel is about to spin');
+  }
+
+  async spin(prize: any) {
+    this.idToLandOn = prize;
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    this.wheel.spin();
+  }
+
+  after() {
+    alert('You have been bamboozled');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxWheelModule } from 'ngx-wheel';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxWheelModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
