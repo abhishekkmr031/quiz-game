4526f108d46e64f5fdb647239dd7d4d8cdc07da4
Harish Kumar Verma <VERM396533@exactsoftware.com>
2023-04-21 16:39:14 +0530

    add css chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { NgxWheelModule } from 'ngx-wheel';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { StartGameComponent } from './start-game/start-game.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, ContentComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ContentComponent, StartGameComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, NgxWheelModule],
   providers: [],
   bootstrap: [AppComponent],
