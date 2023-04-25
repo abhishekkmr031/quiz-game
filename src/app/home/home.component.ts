a7e80f05e63c0e4d8011d9b4483f10dfe2305af0
Harish Kumar Verma <VERM396533@exactsoftware.com>
2023-04-25 13:12:57 +0530

    ugic is added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgxWheelComponent, TextAlignment, TextOrientation } from 'ngx-wheel';
-
+import { Howl } from 'howler';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,6 +14,12 @@ export class HomeComponent {
   idToLandOn: any;
 
   items: any[] = [];
+
+  sound = new Howl({
+    src: [
+      'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    ],
+  });
 
   textOrientation: TextOrientation = TextOrientation.HORIZONTAL;
   textAlignment: TextAlignment = TextAlignment.OUTER;
@@ -33,7 +39,8 @@ export class HomeComponent {
     this.wheel.reset();
   }
   before() {
-    alert('Your wheel is about to spin');
+    console.log(this.sound);
+    this.sound.play();
   }
 
   async spin(prize: any) {
@@ -43,6 +50,7 @@ export class HomeComponent {
   }
 
   after() {
-    alert('You have been bamboozled');
+    console.log(this.wheel.idToLandOn);
+    this.sound.unload();
   }
 }
