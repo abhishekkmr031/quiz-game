98d16f6bb4e20d2f378d3c4cf564e719966b5a8f
Abhishek Kumar <abhishekkmr031@gmail.com>
2023-06-25 18:35:32 +0530

    new wheel component

--- src/app/start-game/start-game.component.ts
+++ src/app/start-game/start-game.component.ts
@@ -1,30 +1,60 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgxWheelComponent, TextAlignment, TextOrientation } from 'ngx-wheel';
 import { Howl } from 'howler';
+
+declare const Winwheel: any;
 
 @Component({
   selector: 'app-start-game',
   templateUrl: './start-game.component.html',
   styleUrls: ['./start-game.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StartGameComponent implements OnInit {
   @ViewChild(NgxWheelComponent, { static: false }) wheel!: NgxWheelComponent;
   @ViewChild('container') container!: ElementRef;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  calcHeight: number = 500;
   names = [
     'Ro Sieben',
-    'Nathan Samsoedien',
-    'Danny Hegeraad',
-    'Bert Kuipers',
-    'Kaushik Sakala',
-    'Bharatgouda Police Patil',
-    'Akshay Kumar',
-    'Eduard Nijensteen',
-    'Eric Edelenbos',
-    'Palvi Jojra',
-    'Eldin Hulsman',
-    'Lex Goudriaan',
-    'Kenneth Mensink',
+    'Nathan',
+    'Danny',
+    'Bert',
+    'Kaushik',
+    'Bharatgouda',
+    'Akshay',
+    'Eduard',
+    'Eric',
+    'Palvi',
+    'Eldin',
+    'Lex',
+    'Kenneth',
   ];
   colors = [
     'red',
@@ -67,9 +97,22 @@ export class StartGameComponent implements OnInit {
     'wine',
   ];
 
+  wheelsegments = [
+    { 'fillStyle': '#eae56f', 'text': 'Prize One' },
+    { 'fillStyle': '#89f26e', 'text': 'Prize Two' },
+    { 'fillStyle': '#7de6ef', 'text': 'Prize Three' },
+    { 'fillStyle': '#e7706f', 'text': 'Prize Four' }
+  ];
+
+  AddWheelSegment(){
+    console.log(this.color);
+    this.wheelsegments.push({'fillStyle':this.color, 'text':this.newName});
+    this.addSegment(this.newName, this.color);
+  }
   idToLandOn: any;
 
-  items: any[] = [];
+  //@Input() item
+  @Input() wheelItems: any[] = [];
   sound = new Howl({
     src: [
       'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
@@ -83,22 +126,95 @@ export class StartGameComponent implements OnInit {
   selectedNames: string[] = [];
   isCollapsibleOpen = false;
   newName!: string;
+  color!:string;
 
-  constructor() {}
+  segments: number = 4;
 
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  theWheel: any;
   ngOnInit(): void {
-    this.assignNames();
+
+    this.theWheel = new Winwheel({
+
+      'canvasId': 'myCanvas',
+      'numSegments': this.segments,
+      'fillStyle': '#e7706f',
+      'lineWidth': 3,
+      'textAlignment': 'outer',
+      'textMargin': 15, // Specify text margin as a number.
+      'textOrientation': 'horizontal',    // Set orientation. horizontal, vertical, curved.
+      'textFontFamily': 'Courier',     // Monospace font best for vertical and curved.
+      'textFontSize': 15,
+      'segments':
+        [
+          { 'fillStyle': '#eae56f', 'text': 'Prize One' },
+          { 'fillStyle': '#89f26e', 'text': 'Prize Two' },
+          { 'fillStyle': '#7de6ef', 'text': 'Prize Three' },
+          { 'fillStyle': '#e7706f', 'text': 'Prize Four' }
+        ],
+
+      'animation':                   // Note animation properties passed in constructor parameters.
+      {
+        'type': 'spinToStop',  // Type of animation.
+        'duration': 5,             // How long the animation is to take in seconds.
+        'spins': 8              // The number of complete 360 degree rotations the wheel is to do.
+      }
+    });
+
+    this.assignNames(this.names);
   }
 
-  assignNames() {
+
+  highlight(n: number) {
+    this.theWheel.segments[3].textFillStyle="blue";
+    this.theWheel.segments[3].textStrokeStyle = "blue";
+    this.theWheel.segments[3].strokeStyle = "blue";
+    //this.deleteSegment(3);
+    // this.addSegment(this.theWheel.segments[3]);
+  }
+
+
+  addSegment(name:string, color:string) {
+    // Use a date object to set the text of each added segment to the current minutes:seconds
+    // (just to give each new segment a different label).
+    let date = new Date();
+
+    // The Second parameter in the call to addSegment specifies the position,
+    // in this case 1 meaning the new segment goes at the start of the wheel.
+    this.theWheel.addSegment({
+      'text': name,
+      'fillStyle': color
+    }, 1);
+
+    // The draw method of the wheel object must be called in order for the changes
+    // to be rendered.
+    this.theWheel.draw();
+  }
+
+  deleteSegment(num:number) {
+    // Call function to remove a segment from the wheel, by default the last one will be
+    // removed; you can pass in the number of the segment to delete if desired.
+    this.theWheel.deleteSegment(num);
+
+    // The draw method of the wheel object must be called to render the changes.
+    this.theWheel.draw();
+  }
+
+  ngOnChanges() {
+    this.assignNames(this.names);
+  }
+
+  assignNames(array1: string[]) {
     this.idToLandOn = Math.floor(Math.random() * this.names.length);
-    this.items = this.names.map((value, index) => ({
+    this.wheelItems = array1.map((value, index) => ({
       fillStyle: this.colors[index % this.colors.length],
       text: value,
       id: index,
       textFillStyle: 'black',
       textFontSize: '22',
     }));
+    this.cdr.detectChanges();
   }
 
   reset() {
@@ -106,16 +222,21 @@ export class StartGameComponent implements OnInit {
   }
 
   spin() {
-    this.removeName(this.idToLandOn);
-    this.reset();
-    this.idToLandOn = Math.floor(Math.random() * this.names.length);
-    this.wheel.spin();
-    this.randomQuestion = '';
+
+    this.theWheel.startAnimation();
+
+    this.highlight(4);
+
+    // this.removeName(this.idToLandOn);
+    // this.reset();
+    // this.idToLandOn = Math.floor(Math.random() * this.names.length);
+    // this.wheel.spin();
+    // this.randomQuestion = '';
   }
 
   removeName(idToLandOn: any) {
     const removedName = this.names.splice(idToLandOn, 1)[0];
-    this.items = this.names.map((value, index) => ({
+    this.wheelItems = this.names.map((value, index) => ({
       fillStyle: this.colors[index % this.colors.length],
       text: value,
       id: index,
@@ -161,6 +282,7 @@ export class StartGameComponent implements OnInit {
     const randomIndex = Math.floor(Math.random() * questions.length);
     this.randomQuestion = questions[randomIndex];
   }
+
   before() {
     this.sound.play();
   }
@@ -171,19 +293,26 @@ export class StartGameComponent implements OnInit {
   editName(id: any) {
     this.newName = this.names[id];
     this.names.splice(id, 1)[0];
-    this.assignNames();
+    this.assignNames(this.names);
   }
 
   deleteName(id: any) {
     this.names.splice(id++, 1)[0];
-    this.assignNames();
+    this.assignNames(this.names);
     this.reset();
     this.idToLandOn = Math.floor(Math.random() * this.names.length);
   }
   addName(name: any) {
-    this.names.push(name);
+    //this.names.push(name);
+    let name1: string[] = [...this.names]
+    console.log(typeof (name1));
+    name1.push(name);
+    this.names = name1;
+    let name2: string[] = [...name1];
+    //name2 = name2.concat(this.names);
     this.reset();
-    this.assignNames();
+    this.assignNames(name1);
+    this.assignNames(name2);
     this.idToLandOn = Math.floor(Math.random() * this.names.length);
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
  theme: {
-   extend: {},
+   extend: {
+    spacing: {
+      '13': '3.25rem',
+      '15': '3.75rem',
+      '128': '32rem',
+      '144': '36rem',
+    }
+   },
  },
  plugins: [],
 };
